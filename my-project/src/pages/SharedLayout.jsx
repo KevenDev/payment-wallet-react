fc302faa9a9eb59e717cd1338b0861bb8caf4c5c
Keven Teles <glevenkeven@gmail.com>
2023-04-15 03:36:06 -0300

    feat: new style in layout

--- my-project/src/pages/SharedLayout.jsx
+++ my-project/src/pages/SharedLayout.jsx
@@ -3,16 +3,14 @@ import { Outlet } from "react-router-dom";
 const SharedLayout = () => {
   return(
   <>
-    <nav className=" font-[Roboto,sans-serif] ">
+    <nav className="w-screen font-[Roboto,sans-serif] mt-5">
       <ul className="flex justify-center gap-3">
         <li>Sacola</li> 
         <li>Pagamento</li>
         <li>Confirmação</li>
       </ul>
     </nav>
-    <div>
       <Outlet/>
-    </div>
   </>
   )
   
