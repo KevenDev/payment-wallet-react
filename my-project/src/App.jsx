ca74becaaf118598a87281807362f88e3b0929de
Keven Teles <glevenkeven@gmail.com>
2023-04-15 03:37:22 -0300

    feat: new Routes and import Components

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import SharedLayout from './pages/SharedLayout'
 import './App.css'
+import Wallet from './Components/Wallet'
+
 
 
 
 function App() {
 
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SharedLayout />}> 
-          
+         <Route index element={<Wallet/>}></Route> 
         </Route>
       </Routes>
     </BrowserRouter>
