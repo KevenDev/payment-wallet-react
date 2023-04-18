957116415f7fbf6252a71f1484c7814f597db3fa
Keven Teles <glevenkeven@gmail.com>
2023-04-18 10:26:07 -0300

    feat: new styles and sizes

--- my-project/src/Components/Wallet.jsx
+++ my-project/src/Components/Wallet.jsx
@@ -18,32 +18,32 @@ const Wallet = () =>{
   },[])
   if(load){
     return(
-      <div
-  class="text-center inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-  role="status">
-  <span
-    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-    >Loading...</span
-  >
-</div>
+      <div className='flex justify-center items-center h-screen'>
+        <div class=" text-center inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"role="status">
+          <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...
+          </span>
+        </div>
+      </div>
     )
   }
     return(
       <>
-        <section className="w-screen h-screen flex-column mt-4  ">
+        <section className="w-screen h-full flex-column mt-4  ">
             {
               data.length > 0 && (
                 <>
-                <div className="h-[50%] w-[100%]flex flex-col justify-center items-center border-2 border-[#CCCCCC] border-solid ">
+                <div className="h-[50%] w-[100%]flex flex-col border-2 border-[#CCCCCC] border-solid ">
                   {data[0].items?.map((items)=>(
-                  <div className="w-full flex justify-between items-center">
-                    <img src={items.product.imageObjects[0].small}  className='' alt="" />
+                  <div className="w-full h-full flex justify-between items-center ">
+                    <img src={items.product.imageObjects[0].small} className='h-[6rem] mb-2' alt="" />
                     <p className='text-xs w-[12rem] font-bold'> {items.product.name}</p>
-                    <span className='font-bold p-1 mr-1'>RS$ {items.product.priceSpecification.price}</span>
+                    <span className='font-bold px-2 mr-1'>RS$ {items.product.priceSpecification.price}</span>
                   </div>
                 ))
               }</div>
-           <div className="w-[90%] flex flex-col py-4 ml-5 text-lg ">
+           <div className=" flex flex-col px-4 py-4 text-lg ">
              <div className='flex justify-between'>
                <p>Produtos</p>
                <p>R$ {data[0].subTotal}</p>
@@ -62,7 +62,7 @@ const Wallet = () =>{
              </div>
           </div>
           <div className='flex justify-center items-center' >
-           <Link to='/cont'> <button className='bg-[#9222DC] h-[3.5rem] w-[90%] rounded-lg text-white p-3 text-xl mt-5'>Seguir para o pagamento</button> </Link>
+        <button className='bg-[#9222DC] h-[3.5rem] w-[80vw] rounded-lg text-white p-3 text-xl mt-5'>Seguir para o pagamento</button> 
           </div>
           </>
         )}
