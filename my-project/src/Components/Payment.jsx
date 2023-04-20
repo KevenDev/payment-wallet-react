a4aaadfacbe993d273f67fae5f20c8d5b26d1f22
Keven Teles <glevenkeven@gmail.com>
2023-04-20 01:47:35 -0300

    feat: new scripts for form

--- my-project/src/Components/Payment.jsx
+++ my-project/src/Components/Payment.jsx
@@ -1,29 +1,45 @@
+import { useState } from "react";
 import { useAtom } from "jotai";
-import { dataAtom } from "../states/StatesAtom";
+import { dataAtom, userCardName, userDateCard, userNumberCard} from "../states/StatesAtom";
 
 const Payment = () => {
   const [data, setData] = useAtom(dataAtom);
+  const [cardNumber, setCardNumber] = useAtom(userNumberCard);
+  const [cardName, SetCardName] = useAtom(userCardName)
+  const [dateCard, setDateCard] = useAtom(userDateCard) 
+
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  } 
+
+  const validateForm = (e) =>{
+  } 
+
+
 
   return (
-    <form className="">
-      <h1>Cartão de Crédito</h1>
-       <div className="flex flex-col">
-        <label>
+    <section className="w-screen h-[90vh] flex flex-col mt-4">
+    <form onSubmit={handleSubmit}>
+      <div className="w-screen h-[60%]  border-2 border-[#CCCCCC] border-solid p-4 font-mediun">
+        <h1 className="text-2xl">Cartão de crédito</h1>
+        <label className="flex flex-col mt-4 text-[#515151]">
           Número:
-          <input type="text" name="name" />
+          <input  onChange={(e)=> setCardNumber(e.target.value)} placeholder="0000 0000 0000 0000" className="h-[2.5rem] p-3 rounded-md border-2 border-[#d6d6d6] border-solid z" type="text" name="name" />
         </label>
-        <label>
+        <label className="flex flex-col mt-4 text-[#515151]">
           Nome do titular do cartão:
-          <input type="text" name="name" />
+          <input onChange={(e) =>SetCardName(e.target.value)} placeholder="Nome impresso no cartão" className=" p-3 rounded-md border-2 border-[#d6d6d6] border-solid h-[2.5rem]" type="text" name="name" />
         </label>
-        <div className="flex" >
+        
+        <div className="flex gap-4 mt-4 text-[#515151]">
           <label>
             Data de validade:
-            <input type="text" name="name" />
+            <input onChange={(e)=> setDateCard(e.target.value)} placeholder="MM/AA" className="h-[2.5rem] p-3 rounded-md border-2 border-[#d6d6d6] border-solid  w-[10rem]" type="text" name="name" />
           </label>
           <label>
             Código CVV:
-            <input type="text" name="name" />
+            <input  placeholder="000" className="h-[2.5rem] rounded-md border-2 border-[#d6d6d6] border-solid p-3  w-[10rem]" type="text" name="name" />
           </label>
         </div>
       </div>
@@ -48,13 +64,14 @@ const Payment = () => {
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <button className="bg-[#9222DC] h-[3.5rem] w-[80vw] rounded-lg text-white p-3 text-xl mt-5">
-              Seguir para o pagamento
+            <button type="submit" className="bg-[#9222DC] h-[3.5rem] w-[80vw] rounded-lg text-white p-3 text-xl mt-5">
+              Finalizar pedido
             </button>
           </div>
         </>
       )}
     </form>
+    </section>
   );
 };
 
