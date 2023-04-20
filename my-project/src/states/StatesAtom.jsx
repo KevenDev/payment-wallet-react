0017037863ba0020134fbfe169566eb36a6506f5
Keven Teles <glevenkeven@gmail.com>
2023-04-20 01:47:05 -0300

    feat: new states global

--- my-project/src/states/StatesAtom.jsx
+++ my-project/src/states/StatesAtom.jsx
@@ -1,5 +1,10 @@
 import { atom } from "jotai";
-  
-
+ 
 const localData = JSON.parse(localStorage.getItem('data'))
 export const dataAtom = atom(localData || [])
+
+export const userNumberCard = atom('')
+export const userCardName = atom('')
+export const userDateCard = atom('')
+
+
