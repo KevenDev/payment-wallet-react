be4ba4412c23b9e9f22c373a688cbb7deef21d3c
Keven Teles <glevenkeven@gmail.com>
2023-04-20 14:17:03 -0300

    feat: validate inputs

--- my-project/src/Components/Payment.jsx
+++ my-project/src/Components/Payment.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAtom } from "jotai";
-import { dataAtom, userCardName, userDateCard, userNumberCard} from "../states/StatesAtom";
+import { dataAtom, userCardName, userCvvCard, userDateCard, userNumberCard} from "../states/StatesAtom";
+import { useNavigate } from "react-router-dom";
 
 const Payment = () => {
   const [data, setData] = useAtom(dataAtom);
@@ -8,44 +9,66 @@ const Payment = () => {
   const [cardName, SetCardName] = useAtom(userCardName)
   const [dateCard, setDateCard] = useAtom(userDateCard)
   const [isValid, setIsValid] = useState(true);
-
-  const span = document.querySelectorAll('.span')
+  const [cvc, setCvc] = useAtom(userCvvCard)
+  const navigate = useNavigate();
 
   const handleSubmit = (e) =>{
-    validateForm()
-    e.preventDefault()
+    e.preventDefault();
+    validateForm();
   } 
 
-  const validateForm = (e) =>{
-    cardNumber.length != 16 ? setIsValid(false) : setIsValid(true)  
-  } 
+  const validateForm = () =>{
+    let isValidForm = true;
+  
+    if (cardNumber.length !== 16) {
+      setIsValid(false);
+      isValidForm = false;
+    }
 
+    if (cardName.length === 0) {
+      setIsValid(false);
+      isValidForm = false;   
+    }
+
+    if(dateCard.length != 4){
+      setIsValid(false);
+      isValidForm = false;   
+    }
+    if(cvc.length != 3){
+      setIsValid(false);
+      isValidForm = false;   
+    }
+
+    isValidForm ? navigate("/confirmPayment") : setIsValid(false);
+  } 
 
   return (
-    <section className="w-screen h-[90vh] flex flex-col mt-4">
+    <section className="w-screen h-[90vh] flex flex-col mt-2">
     <form onSubmit={handleSubmit}>
       <div className="w-screen h-[60%]  border-2 border-[#CCCCCC] border-solid p-4 font-mediun">
         <h1 className="text-2xl">Cartão de crédito</h1>
-        <label className="flex flex-col mt-4 text-[#515151]">
+        <label className="flex flex-col mt-1 text-[#515151]">
           Número:
-          <input onChange={(e)=> setCardNumber(e.target.value)} placeholder="0000 0000 0000 0000"  className={`h-[2.5rem] p-3 rounded-md border-2 ${isValid ? 'border-[#d6d6d6]' : 'border-red-500'} border-solid`} 
+          <input maxLength={16} onChange={(e)=> setCardNumber(e.target.value)} placeholder="0000 0000 0000 0000"  className={`h-[2.5rem] p-3 rounded-md border-2 ${isValid ? 'border-[#d6d6d6]' : 'border-red-500'} border-solid`} 
           type="text" />
           {isValid ? null: <span className="text-red-500">insira um número de cartão válido</span> }
         </label>
         <label className="flex flex-col mt-4 text-[#515151]">
           Nome do titular do cartão:
           <input onChange={(e) =>SetCardName(e.target.value)} placeholder="Nome impresso no cartão" className=" p-3 rounded-md border-2 border-[#d6d6d6] border-solid h-[2.5rem]" type="text" name="name" />
-          <span className="">insira um nome cartão válido</span>
+          {isValid ? null: <span className=" text-red-500">insira um número de cartão válido</span> }
         </label>
         
         <div className="flex gap-4 mt-4 text-[#515151]">
           <label>
             Data de validade:
-            <input onChange={(e)=> setDateCard(e.target.value)} placeholder="MM/AA" className="h-[2.5rem] p-3 rounded-md border-2 border-[#d6d6d6] border-solid  w-[10rem]" type="text" name="name" />
+            <input maxLength={4} onChange={(e)=> setDateCard(e.target.value)} placeholder="MM/AA" className="h-[2.5rem] p-3 rounded-md border-2 border-[#d6d6d6] border-solid  w-[10rem]" type="text" name="name" />
+            {isValid ? null: <span className="text-red-500">insira um número de cartão válido</span> }
           </label>
           <label>
             Código CVV:
-            <input  placeholder="000" className="h-[2.5rem] rounded-md border-2 border-[#d6d6d6] border-solid p-3  w-[10rem]" type="text" name="name" />
+            <input maxLength={3} onChange={(e)=>setCvc(e.target.value)} placeholder="000" className="h-[2.5rem] rounded-md border-2 border-[#d6d6d6] border-solid p-3  w-[10rem]" type="text" name="name" />
+            {isValid ? null: <span className="text-red-500">insira um número de cartão válido</span> }
           </label>
         </div>
       </div>
