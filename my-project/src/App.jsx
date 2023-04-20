8a441fa1892ee28e385b5c9eeee1a775336faec3
Keven Teles <glevenkeven@gmail.com>
2023-04-20 14:16:20 -0300

    feat: new routs in app

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,6 +3,7 @@ import SharedLayout from './pages/SharedLayout'
 import './App.css'
 import Wallet from './Components/Wallet'
 import Payment from './Components/Payment'
+import Confirmation from './Components/Confirmation'
 
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<SharedLayout />}> 
          <Route index element={<Wallet/>}></Route>
          <Route path='/payment' element={<Payment/>}> </Route> 
+         <Route path='/confirmPayment' element={<Confirmation/>}> </Route>
         </Route>
       </Routes>
     </BrowserRouter>
