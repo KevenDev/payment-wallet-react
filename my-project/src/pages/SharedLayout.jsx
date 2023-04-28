ef15e30fa7716ec123164807f7fe9088009ff557
Keven Teles <glevenkeven@gmail.com>
2023-04-27 22:38:28 -0300

    feat: added border for routes pages

--- my-project/src/pages/SharedLayout.jsx
+++ my-project/src/pages/SharedLayout.jsx
@@ -1,13 +1,21 @@
-import { Outlet,Link } from "react-router-dom";
+import { Outlet,Link,useLocation } from "react-router-dom";
 
 const SharedLayout = () => {
+  const location = useLocation();
+
   return(
   <>
     <nav className="md:text-2xl w-screen font-[Roboto,sans-serif] mt-5">
       <ul className="flex justify-center gap-3">
-      <Link to='/'>  <li>Sacola</li> </Link>
-        <li>Pagamento</li>
-        <li>Confirmação</li>
+      <Link to="/">
+            <li className={location.pathname === "/" ? "border-b-2 border-black" : ""}>Sacola</li>
+          </Link>
+          <Link to="/">
+            <li className={location.pathname === "/payment" ? "border-b-2 border-black" : ""}>Pagamento</li>
+          </Link>
+          <Link to="/">
+            <li className={location.pathname === "/confirmPayment" ? "border-b-2 border-black" : ""}>Confirmação</li>
+          </Link>
       </ul>
     </nav>
       <Outlet/>
