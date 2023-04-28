d3bf49e1f705901eb401f8148440a75a60d0b65a
Keven Teles <glevenkeven@gmail.com>
2023-04-27 22:31:36 -0300

    feat: add responsive for confirmation

--- my-project/src/Components/Confirmation.jsx
+++ my-project/src/Components/Confirmation.jsx
@@ -12,7 +12,7 @@ const Confirmation = ()=>{
 
     return(
         <>
-        <section className="w-screen h-[100%] mt-4 ">
+        <section className="md:w-[40%] md:mx-auto w-screen h-[100%] mt-4 ">
         <div className="flex flex-col p-4 justify-center items-center border-2 border-[#CCCCCC] border-solid">
             <h1 className="text-2xl"> Compra Efetuada com sucesso</h1>
             <p className="text-xl text-[#515151]">{`****.****.****.${cardNumber.slice(12,16)}`}</p>
@@ -52,8 +52,8 @@ const Confirmation = ()=>{
               <p className="font-bold">R$ {data[0].total}</p>
              </div>
           </div>
-          <div className='flex justify-center items-center' >
-          <Link to='/'> <button className='bg-[#000000] h-[3.5rem] w-[80vw] rounded-lg text-white p-3 text-xl mt-5'>Voltar ao início do protótipo</button> </Link>
+          <div className='md:w-[50%] md:mx-auto flex justify-center items-center' >
+          <Link to='/'> <button className='bg-[#000000] h-[3.5rem] md:w-[40vw] w-[80vw] rounded-lg text-white p-3 text-xl mt-5'>Voltar ao início do protótipo</button> </Link>
           </div>
           </>
         )}
